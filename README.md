5c8f49625fd389f33fce41648ec7e92eecfdf7d7
Octav Sandulescu <github@narc.ro>
2012-12-15 21:41:23 +0000

    Everything needed to run another copy of narc.ro
    ...except the content.

---
+++ README.md
@@ -0,0 +1,7 @@
+This is the code behind [Narc Dot Ro](http://www.narc.ro/). It is made available in the hope it will be useful to somebody, at some point.
+
+Of note is that the website itself is quite tiny and readable: though a few edits may need to be made for a different website, it's easy to find where the edits are to be made, and what they should be. This is the quintessential example of straightforwardness, in my opinion.
+
+Also of note is this is intended for a small site: the administration page would become fairly unwieldy with large numbers of articles and/or large numbers of available downloads. This may be improved, in time, if it proves necessary. For now, it serves my purposes wonderfully.
+
+You are welcome to consider the work Public Domain, or otherwise under the least strict licensing terms available in your legal area. Do with it as you will, I'm just making the thing.
---
+++ index.php
@@ -0,0 +1,56 @@
+<?PHP
+header('Content-Type: text/html; charset=UTF-8');
+$page_timer_start = microtime();
+
+define('PROG_NAME',  'Narc Dot Ro');
+define('PROG_VER',   '3.0');
+define('PROG_FULL',  PROG_NAME.'/'.PROG_VER);
+
+define('PATH_BASE',  dirname(__FILE__));
+define('PATH_INC',   PATH_BASE.'/inc');
+define('PATH_TPL',   PATH_BASE.'/tpl');
+define('PATH_PAGES', PATH_BASE.'/pages');
+
+define('PROG_URL',   'http://www.narc.ro');
+define('CSS_URL',    PROG_URL.'/css');
+define('JS_URL',     PROG_URL.'/js');
+
+define('DEBUG_LOG',  PATH_BASE.'/debug.log');
+
+include(PATH_INC.'/conf.inc');
+include(PATH_INC.'/sql.inc');
+include(PATH_INC.'/strings.inc');
+include(PATH_INC.'/ua.inc');
+include(PATH_INC.'/markdown.php');
+
+$sql = new SQLEngine();
+
+if(!$sql->health_check())
+	die('Damn, something went horrifically wrong!');
+
+$_ARGS = array();
+if(empty($_REQUEST['args']))
+	$_ARGS[0] = 'home';
+else
+	$_ARGS = explode('/', $_REQUEST['args']);
+
+$page_uri = $_ARGS[0];
+
+if(is_file(PATH_PAGES.'/'.$page_uri.'.inc')) {
+	require(PATH_PAGES.'/'.$page_uri.'.inc');
+	die();
+}
+
+$page = $sql->fetch_page($page_uri);
+if(empty($page)) {
+	require(PATH_PAGES.'/404.inc');
+	die();
+}
+echo header_text($page_uri, $page['title']);
+echo Markdown($page['content']);
+echo '<!-- End of the markdown stuff --></div>';
+
+echo sidebar_text($page_uri);
+echo footer_text();
+
+?>
